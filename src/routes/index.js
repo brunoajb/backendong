4e8373abfa61fd229bb6c2665202d41e9294a357
brunoajb <58607891+brunoajb@users.noreply.github.com>
2020-06-07 23:44:52 -0300

    api

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express=require('express');
 const routes = express.Router();
 
 routes.get('/', (req,res)=>{
-  return res.send('<h1>Api Artêmis | Todos por um!</h1>')
+  return res.send('Api Artêmis | Todos por um!')
 });
 
 module.exports = routes;
